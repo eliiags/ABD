e6ab5c6cc00e877a447540634157b528c90abeb2
eliiags <eaguero@ucm.es>
2017-05-30 04:57:07 +0200

    borrado todo

--- js/goTo.js
+++
@@ -1,8 +0,0 @@
-/*
-document.getElementById("boton_iniciar").onclick = function(){
-  location.href='login2.html';
-};
-
-document.getElementById("boton_registro").onclick = function(){
-  location.href="registro.html";
-};*/
